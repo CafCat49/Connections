b0e322912c975353e54e4ba6365942740cc2d8bb
Christina A. H. <47138990+CafCat49@users.noreply.github.com>
2026-05-18 15:34:20 +0200

    Teleport mechanic added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,13 +4,45 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 0;
-    
+    public float shiftOffset = 1;
+    public GameObject ShiftPoint;
+
+    [SerializeField] private InputAction interactAction;
     private Rigidbody rb;
     private float moveX, moveY;
+    private bool isShiftPlaced;
+    
+    private void OnEnable() => interactAction.Enable();
+    private void OnDisable() => interactAction.Disable();
     
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        isShiftPlaced = false;
+    }
+
+    void Update()
+    {
+        if (interactAction.triggered)
+        {
+            if (!ShiftPoint) return;
+            if (!isShiftPlaced)
+            {
+                Vector3 shiftPos = new Vector3(
+                    transform.position.x, 
+                    transform.position.y + shiftOffset, 
+                    transform.position.z
+                );
+                
+                ShiftPoint.transform.position = shiftPos;
+                isShiftPlaced = true;
+            }
+            else
+            {
+                transform.position = ShiftPoint.transform.position;
+                isShiftPlaced = false;
+            }
+        }
     }
 
     void FixedUpdate()
