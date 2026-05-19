33df28953a7b9cf8e18382c80ed68b638b5cfd74
Christina A. H. <47138990+CafCat49@users.noreply.github.com>
2026-05-19 04:49:23 +0200

    Added pause function and extra tweaks

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,38 +6,47 @@ public class PlayerController : MonoBehaviour
     public float speed = 0;
     public float shiftOffset = 1;
     public GameObject shiftPoint;
+    public GameObject pauseWarningMsg;
 
     [SerializeField] private InputAction planarShiftAction;
     [SerializeField] private InputAction respawnAction;
+    [SerializeField] private InputAction pauseAction;
     
     private Rigidbody rb;
     private float moveX, moveY;
     private bool isShiftPlaced;
     private Vector3 checkpoint, spawnpoint;
+    private bool disableManualPause = false;
+    private bool isPaused;
 
     private void OnEnable()
     {
         planarShiftAction.Enable();
         respawnAction.Enable();
+        pauseAction.Enable();
     }
 
     private void OnDisable()
     {
         planarShiftAction.Disable();
         respawnAction.Disable();
+        pauseAction.Disable();
     }
     
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         isShiftPlaced = false;
+        pauseWarningMsg.SetActive(false);
         checkpoint = Vector3.zero;
         spawnpoint = transform.position; //TODO: update when starting a new level
     }
 
     void Update()
     {
-        if (Time.timeScale == 0) return; //prevents usage of abilities if game is paused
+        if (pauseAction.triggered && !disableManualPause) Pause();
+        
+        if (isPaused) return; //prevents usage of abilities if game is paused
         if (planarShiftAction.triggered) PlanarShift();
         if (respawnAction.triggered) Respawn();
     }
@@ -69,10 +78,44 @@ public class PlayerController : MonoBehaviour
         rb.linearVelocity = Vector3.zero;
         rb.angularVelocity = Vector3.zero;
         transform.position = spawnpoint;
+        isShiftPlaced = false;
+    }
+
+    public void Pause(bool forcePause = false, bool forceStart = false)
+    {
+        if (forcePause)
+        {
+            Time.timeScale = 0;
+            disableManualPause = true;
+            isPaused = true;
+            return;
+        }
+        
+        if (forceStart)
+        {
+            Time.timeScale = 1;
+            disableManualPause = false;
+            isPaused = false;
+            return;
+        }
+        
+        if (!isPaused)
+        {
+            pauseWarningMsg.SetActive(true);
+            Time.timeScale = 0;
+            isPaused = true;
+        }
+        else
+        {
+            pauseWarningMsg.SetActive(false);
+            Time.timeScale = 1;
+            isPaused = false;
+        }
     }
 
     void FixedUpdate()
     {
+        if (isPaused) return;
         Vector3 movement = new Vector3(moveX, 0.0f, moveY);
         rb.AddForce(movement * speed);
     }
--- Assets/Scripts/TutorialManager.cs
+++ Assets/Scripts/TutorialManager.cs
@@ -5,25 +5,28 @@ public class TutorialManager : MonoBehaviour
 {
     public GameObject tutorialWindow;
     public TextMeshProUGUI tutorialText;
-
+    public PlayerController pc;
+    
     void Start()
     {
         ShowTutorial("Controls:\n" +
                       "WASD to move\n" +
                       "F to use Planar Shift\n" +
-                      "R to Respawn");
+                      "R to Respawn\n" +
+                      "SPACE to Pause\n" +
+                      "\n(Close tutorial to begin)");
     }
     
     public void ShowTutorial(string message)
     {
+        pc.Pause(true);
         tutorialText.text = message;
         tutorialWindow.SetActive(true);
-        Time.timeScale = 0;
     }
     
     public void HideTutorial()
     {
+        pc.Pause(false, true);
         tutorialWindow.SetActive(false);
-        Time.timeScale = 1;
     }
 }
