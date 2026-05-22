07a3a0b58910a66e4cbb782b8f313d6abc191b63
Christina A. H. <47138990+CafCat49@users.noreply.github.com>
2026-05-22 14:53:17 +0200

    evil puzzle

--- Assets/Scripts/BridgeTrigger.cs
+++ Assets/Scripts/BridgeTrigger.cs
@@ -2,21 +2,52 @@ using UnityEngine;
 
 public class BridgeTrigger : MonoBehaviour
 {
+    public Vector3 targetEulerAngles;
+    public float rotationDuration = 5.0f;
     public GameObject bridgePivot;
-    private bool isTriggered;
+
+    private Vector3 baseEulerAngles;
+    private Quaternion startRotation;
+    private Quaternion targetRotation;
+    private float elapsedTime = 0f;
+    private bool isRotating = false;
 
     private void Start()
     {
-        isTriggered = false;
+        startRotation = bridgePivot.transform.rotation;
+        baseEulerAngles = bridgePivot.transform.eulerAngles;
+    }
+
+    private void Update()
+    {
+        if (isRotating)
+        {
+            elapsedTime += Time.deltaTime;
+            float percentage = Mathf.Clamp01(elapsedTime / rotationDuration);
+            bridgePivot.transform.rotation = Quaternion.Lerp(startRotation, targetRotation, percentage);
+            if (percentage >= 1f) isRotating = false;
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (isTriggered)  return;
         if (other.CompareTag("Player"))
         {
-            bridgePivot.transform.Rotate(Vector3.right, 90);
-            isTriggered = true;
+            targetRotation = Quaternion.Euler(targetEulerAngles);
+            startRotation = bridgePivot.transform.rotation;
+            elapsedTime = 0f;
+            isRotating = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            targetRotation = Quaternion.Euler(baseEulerAngles);
+            startRotation = bridgePivot.transform.rotation;
+            elapsedTime = 0f;
+            isRotating = true;
         }
     }
 }
